f5fca3ca6427d85e2ea3e0424103aec253e7508c
Tim Green <tgreen@opscode.com>
2013-04-16 15:52:51 -0400

    bump the patch version

--- cookbooks/csinstaller/metadata.rb
+++ cookbooks/csinstaller/metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'chirag@clogeny.com'
 license          'All rights reserved'
 description      'Installs/Configures Citrix CloudPlatform 3.0.6'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '0.1.2'
 recipe            'cs_installer:mgmt_server', "Setup the Management Server correctly"
 recipe            'cs_installer:setup_zone', "Setup the CloudPlatform Environment(Zone)"
 
